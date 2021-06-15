1cf3b8f370dcb7ff3d9e729c675b972a37b5ace3
ckpri <ckpri@192.168.254.26>
2021-06-15 09:49:31 -0400

    second commit

--- Practice1/src/first/layout.java
+++ Practice1/src/first/layout.java
@@ -5,6 +5,7 @@ public class layout {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("connecting to github");
+		System.out.println("connecting githb to jenkins server");
 	}
 
 }
